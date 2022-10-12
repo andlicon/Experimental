95ecfc78a0fc4dd6d37e376edde5adb3b0905bd9
Andlicon <andresjzabalac@hotmail.com>
2022-10-12 15:40:32 -0400

    Inicio del proyecto

---
+++ php/login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Login</title>
+
+    <link rel="stylesheet" href="/css/main.css">
+</head>
+<body class="body-login">
+    <div class="login">
+        <h2 class="login__titulo">Acceda al sistema.</h2>
+        <img class="login__imagen" src="#" alt="Logo del colegio">
+        <div class="login__boton">
+            <div class="boton">
+
+            </div>
+        </div>
+        <form class="login__formulario formulario">
+            <input class="formulario__input" type="text" placeholder="usuario">
+            <input class="formulario__input" type="password" placeholder="contraseña">
+            <button class="formulario__boton">Entrar</button>
+        </form>
+    </div>
+</body>
+</html>
