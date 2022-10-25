e2e7f88e3c4e10e6281d80e349b0f3ebf3f021ac
Andlicon <andresjzabalac@hotmail.com>
2022-10-25 19:19:47 -0400

    Se modularizo la accion de actualizar

---
+++ php/acciones/Actualizar.php
@@ -0,0 +1,27 @@
+<?php 
+    include_once('../conexion/IModificador.php');
+
+    class Actualizar {
+        private $dao;
+        private $pagina;
+        private $objSerializar;
+
+        public function __construct(IConsultor $dao, $pagina, $objSerializar) {
+            $this->dao = $dao;
+            $this->pagina = $pagina;
+            $this->objSerializar = $objSerializar;
+        }
+
+        public function actualizar() {
+            try {
+                $resultado = $this->dao->getTodos();
+
+                $serialize = serialize($resultado);
+                header($this->pagina.'?'.$this->objSerializar.'='.urlencode($serialize));
+            }
+            catch(Exception $e) {
+                echo $e;
+            }
+        }
+    }
+?>
--- php/interfazRepresentante/accionBoton/actualizarRep.php
+++ php/interfazRepresentante/accionBoton/actualizarRep.php
@@ -1,24 +1,16 @@
 <?php
-    include_once('../instancias/Representante.php');
     include_once('../conexion/RepresentanteConsul.php');
     include_once('../formulario/Alerta.php');
     include_once('../general/crearCedula.php');
+    include_once('../acciones/Actualizar.php');
 
     $pagina = "Location: RepresentanteView.php";
+    $objSerializar = "representantes";
 
     if( isset($_POST['actualizar']) ) {
-        try {   //Extraer informacion de la base de datos
             $bd = new BaseDeDatos('127.0.0.1:3306', 'mysql', 'Experimental', 'root', '');
             $representanteConsul = new RepresentanteConsul($bd);
-            $representanteConsul = $representanteConsul->getTodos();
-
-            //Serializar el objeto para poderlo pasar a la vista resultado
-            $serialize = serialize($representanteConsul);
-            
-            header("$pagina?representantes=".urlencode($serialize));
-        }
-        catch(Exception $mensaje) {  
-            alerta($mensaje);
-        }
+            $actualizador = new Actualizar($representanteConsul, $pagina, $objSerializar);
+            $actualizador->actualizar();
     }
 ?>
