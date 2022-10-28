0002e9a7a8d2b339979808d304ffc390f4e0eb7e
Andlicon <andresjzabalac@hotmail.com>
2022-10-28 06:57:27 -0400

    Solo se pueden modificar 1 checkbox

--- php/InterfazEstudiante/accionBoton/modificarEstu.php
+++ php/InterfazEstudiante/accionBoton/modificarEstu.php
@@ -9,35 +9,38 @@
 
             $checks = $_POST['check'];
 
-            $nombre = $_POST['nombreInput'];
-            $apellido = $_POST['apellidoInput'];
-            $fecha = $_POST['fechaInput'];
-            $idClase = $_POST['claseInput'];
+            if(count($checks)==1) {
+                $nombre = $_POST['nombreInput'];
+                $apellido = $_POST['apellidoInput'];
+                $fecha = $_POST['fechaInput'];
+                $idClase = $_POST['claseInput'];
 
-            $nacionalidadInput = $_POST['nacionalidadInput'];
-            $cedulaInput = $_POST['cedulaInput'];
-            $cedula = $cedulaInput=="" ? "" : crearCedula($nacionalidadInput, $cedulaInput);
+                $nacionalidadInput = $_POST['nacionalidadInput'];
+                $cedulaInput = $_POST['cedulaInput'];
+                $cedula = $cedulaInput=="" ? "" : crearCedula($nacionalidadInput, $cedulaInput);
 
-            try {   //Extraer informacion de la base de datos
-                $bd = new BaseDeDatos('127.0.0.1:3306', 'mysql', 'Experimental', 'root', '');
-                $estudianteDAO = new EstudianteDAO($bd);
-                $representanteConsul = new RepresentanteConsul($bd);
+                try {   //Extraer informacion de la base de datos
+                    $bd = new BaseDeDatos('127.0.0.1:3306', 'mysql', 'Experimental', 'root', '');
+                    $estudianteDAO = new EstudianteDAO($bd);
+                    $representanteConsul = new RepresentanteConsul($bd);
 
-                $modificador = new ModificarEstudiante($estudianteDAO, $representanteConsul, $pagina, $objSerializar);
+                    $modificador = new ModificarEstudiante($estudianteDAO, $representanteConsul, $pagina, $objSerializar);
 
-                for($i=0; $i<count($checks); $i++) {
-                    $idEstudiante = $checks[$i];
-                    $modificador->modificar(array(
-                                                    array($cedula),
-                                                    array($nombre, $apellido, $fecha, $idClase, 
-                                                            $idEstudiante)
-                                                ));
+                    for($i=0; $i<count($checks); $i++) {
+                        $idEstudiante = $checks[$i];
+
+                        $modificador->modificar(array(
+                                                        array($cedula),
+                                                        array($nombre, $apellido, $fecha, $idClase, 
+                                                                $idEstudiante)
+                                                    ));
+                    }
+                
+                    // header($pagina);
                 }
-    
-                // header($pagina);
-            }
-            catch(Exception $mensaje) {  
-                alerta($mensaje);
+                catch(Exception $mensaje) {  
+                    alerta($mensaje);
+                }
             }
         }
     }
