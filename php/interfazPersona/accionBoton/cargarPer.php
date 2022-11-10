864b8359df1856085234c82ae61aa0d8eb08f277
Andlicon <andresjzabalac@hotmail.com>
2022-11-10 11:34:44 -0400

    Se arreglo cargarPer para la nueva implementacion del DAO

--- php/interfazPersona/accionBoton/cargarPer.php
+++ php/interfazPersona/accionBoton/cargarPer.php
@@ -1,5 +1,4 @@
 <?php
-    include_once('../acciones/CargarPersona.php');
     //Clases  de acceso a datos
     include_once('../conexion/PersonaDAO.php');
     include_once('../conexion/ContactoDAO.php');
@@ -16,32 +15,23 @@
     FALTA ALGUN METODO PARA DESACER LOS CAMBIOS DE HABER OCURRIDO ALGUN ERRROR
 */
     if( isset($_POST['cargar']) ) {
-        $pagina = 'Location: personaView.php';
+        $pagina = new Pagina(Pagina::PERSONA);
 
         //Comprobando los inputs
         $nacionalidadInput = comprobarInput('nacionalidadInput', 'Se debe introducir una nacionalidad valida', $pagina);
         $cedulaInput = comprobarInput('cedulaInput', 'Se debe introducir un numero de cedula valido', $pagina);
+        $cedula = crearCedula($nacionalidadInput, $cedulaInput);    //se crea la cedula
         $nombre = comprobarInput('nombreInput', 'Se debe introducir un nombre valido', $pagina);
         $apellido = comprobarInput('apellidoInput', 'Se debe introducir un apellido valido', $pagina);
-
-        $cedula = crearCedula($nacionalidadInput, $cedulaInput);    //se crea la cedula
 
         try { 
             $bd = new BaseDeDatos('127.0.0.1:3306', 'mysql', 'Experimental', 'root', '');
                 //PERSONA
             $personaDAO = new PersonaDAO($bd);
+            $personaDAO->cargar(array($cedula, $nombre, $apellido));
 
-            $cargador = new CargarPersona($personaDAO, $contactoDAO);
-            $cargador->cargar(array
-                                    (
-                                        array($cedula, $nombre, $apellido, 1),
-                                        array($cedula, 1, $correo), 
-                                        array($cedula, 2, $telefono)
-                                    )
-                            );
-
-            $alerta = new Mensaje(null, true, 'se ha cargado exitosamente al representante');
-            mandarMensaje($alerta, $pagina);
+            $bd->guardarCambios();
+            $pagina->imprimirMensaje(null, Mensaje::EXITO, "Se ha cargado a la persona exitosamente.");
         }
         catch(Exception $e) {  
             alerta($e);
