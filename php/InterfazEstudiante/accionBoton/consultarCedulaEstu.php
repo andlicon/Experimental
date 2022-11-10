dbaebebca53fadeeceeaf8d53bdd19728b0d831e
Andlicon <andresjzabalac@hotmail.com>
2022-11-10 12:50:30 -0400

    Consulta por cedula acorde a la nueva implementacion DAO

--- php/InterfazEstudiante/accionBoton/consultarCedulaEstu.php
+++ php/InterfazEstudiante/accionBoton/consultarCedulaEstu.php
@@ -1,27 +1,29 @@
 <?php
     include_once('../conexion/EstudianteDAO.php');
-    include_once('../acciones/Consultar.php');
+    include_once('../excepciones/InputException.php');
     include_once('../general/comprobarInput.php');
     include_once('../general/crearCedula.php');
+    include_once('../general/Pagina.php');
 
     /*
         Consulta estudiantes dependiendo de la clase.
     */
     if( isset($_POST['consultar-cedula']) ) {
-        $pagina = "Location: estudianteView.php";
-        $objSerializar = "estudiantes";
-        
-        $nacionalidadInput = comprobarInput('nacionalidadInput', 'Se debe introducir una nacionalidad valida', $pagina);
-        $cedulaInput = comprobarInput('cedulaInput', 'Se debe introducir un numero de cedula valido', $pagina);
-        $cedula = crearCedula($nacionalidadInput, $cedulaInput);
+        $pagina = new Pagina(Pagina::ESTUDIANTE);
 
-        try {   //Extraer informacion de la base de datos
+        try {  
+            $nacionalidadInput = comprobarInput('nacionalidadInput', $pagina);
+            $cedulaInput = comprobarInput('cedulaInput', $pagina);
+            $cedula = crearCedula($nacionalidadInput, $cedulaInput);
+
             $bd = new BaseDeDatos('127.0.0.1:3306', 'mysql', 'Experimental', 'root', '');
             $estudianteDAO = new EstudianteDAO($bd);
 
             $resultado = $estudianteDAO->getInstanciaCedula(array($cedula));
-            $serialize = serialize($resultado);
-            header($pagina.'?'.$objSerializar.'='.urlencode($serialize));
+            $pagina->actualizarPagina($resultado);
+        }
+        catch(InputException $e) {
+            $e->imprimirError();
         }
         catch(Exception $e) {  //No se ha podido conectar a la bd
             echo $e;
