2655fac544227e6a17401fe3b15147624219423a
Andlicon <andresjzabalac@hotmail.com>
2022-11-11 12:13:55 -0400

    a

--- php/interfazDeuda/accionBoton/cobrarMensualidad.php
+++ php/interfazDeuda/accionBoton/cobrarMensualidad.php
@@ -13,6 +13,7 @@
                 $bd = new BaseDeDatos('127.0.0.1:3306', 'mysql', 'Experimental', 'root', '');
                 $bd->procedure("CALL cobrar_mensualidad(NOW(), $monto);", null);
 
+                $bd->guardarCambios();
                 $pagina->imprimirMensaje(null, Mensaje::EXITO, "Se cobro la mensualidad con exito.");
             }
             catch(Exception $e) {   //De no conectarse a la bd
