3cf4fc94d3bed0575473dc057094514f210e52d1
Andlicon <andresjzabalac@hotmail.com>
2022-11-26 13:01:01 -0400

    Se logro el sticky la puta madre

--- php/Interfaces/opcion/view.php
+++ php/Interfaces/opcion/view.php
@@ -1,9 +1,12 @@
 <!DOCTYPE html>
-<html lang="en" class="vista">
+<html lang="en">
 
 <?php
     include_once('../ruta.php');
     include('redireccionarPagina.php');
+    include_once(GENERAL_PATH.'deserializarUsuario.php');
+
+    $usuario = deserializarUsuario();
 ?>
 
 <head>
@@ -15,18 +18,19 @@
 
     <link rel="stylesheet" href="/css/main.css">
 </head>
-<body class="vista__contenido">
-    <h2 class="vista__titulo">Opciones</h2>
-    <div class="vista__cuerpo">
+<body class="body-page">
+    <div class="usuario">
+
+    </div>
+    <div class="body-main">
+        <h2 class>Opciones</h2>
         <form action="" method="POST" class="vista__form">    
             <!-- botones -->
             <div class="botones">
                 <h2 class="botones__titulo">Acciones</h2>
                 <?php
-                    include_once(GENERAL_PATH.'deserializarUsuario.php');
                     include_once('getPermiso.php');
 
-                    $usuario = deserializarUsuario();
                     $permiso = getPermiso($usuario);
                     generarBotones($permiso);
                 ?>
