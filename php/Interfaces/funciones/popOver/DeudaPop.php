257bc43bc2c11922dfc6e9793052024fa00e4603
Andlicon <andresjzabalac@hotmail.com>
2022-12-10 10:47:02 -0400

    a

--- php/Interfaces/funciones/popOver/DeudaPop.php
+++ php/Interfaces/funciones/popOver/DeudaPop.php
@@ -1,6 +1,7 @@
 <?php
     include_once(FUNCIONES_IG_PATH.'popOver/GenerarPopOver.php');
     include_once(DAO_PATH.'/ClaseConsul.php');
+    include_once(DAO_PATH.'/MotivoConsul.php');
     include_once(DAO_PATH.'/BaseDeDatos.php');
     include_once(DTO_PATH.'/Deuda.php');
 
@@ -14,28 +15,40 @@
             $resultado = $this->consultor->getInstancia(array($id));
             $deuda = $resultado[0];
 
-            $cedula;
-            $idEstudiante;
-            $fecha;
-            $montoInicial;
+            $deudaId = $deuda->getId();
+            $cedula = $deuda->getCedula();
+            $idEstudiante = $deuda->getIdEstudiante();
+            $fecha = $deuda->getFecha();
+            $montoInicial = $deuda->getMontoInicial();
+            $descripcion = $deuda->getDescripcion();
+            $idMotivo = $deuda->getidMotivo();
+
+            $bd = new BaseDeDatos('127.0.0.1:3306', 'mysql', 'Experimental', 'root', '');
+
+            //Motivo
+            $motivoConsul = new MotivoConsul($bd);
+            $resultado = $motivoConsul->getInstancia(array($idMotivo));
+            $motivo = $resultado[0]->getDescripcion();
+
+            $deudaDescripcion = $descripcion==null ? $motivo : $motivo.': '.$descripcion;
 
             //Estudiante
-            $bd = new BaseDeDatos('127.0.0.1:3306', 'mysql', 'Experimental', 'root', '');
             $estudianteDAO = new EstudianteDAO($bd);
             $resultado = $estudianteDAO->getInstancia(array($idEstudiante));
             $estudianteNombre = $resultado[0]->getNombre();
 
             $popOver = 
             "<div class=\"popOver\">
-                <span class=\"popOver__trigger\">$nombre</span>
+                <span class=\"popOver__trigger\">$deudaId</span>
                 <div class=\"popOver__contenido\">";
                     
             $popOver = $popOver.
             "       <span class=\"popOver__informacion\">Informacion</span>
-                    <span class=\"popOver__elemento\">$cedula</span>
-                    <span class=\"popOver__elemento\">$estudianteNombre</span>
-                    <span class=\"popOver__elemento\">$fecha</span>
-                    <span class=\"popOver__elemento\">$montoInicial</span>
+                    <span class=\"popOver__elemento\">Cedula: $cedula</span>
+                    <span class=\"popOver__elemento\">Estudiante: $estudianteNombre</span>
+                    <span class=\"popOver__elemento\">Fecha: $fecha</span>
+                    <span class=\"popOver__elemento\">Monto inicial: $montoInicial</span>
+                    <span class=\"popOver__elemento\">$deudaDescripcion</span>
                 </div>
             </div>";
 
