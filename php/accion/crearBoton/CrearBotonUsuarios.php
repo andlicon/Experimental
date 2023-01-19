2ace1e2b0ac03353900c07ba4646eac2dbce3e49
Andlicon <andresjzabalac@hotmail.com>
2023-01-19 15:50:00 -0400

    Se anadio un nuevo campo en las consultas de usuario

--- php/accion/crearBoton/CrearBotonUsuarios.php
+++ php/accion/crearBoton/CrearBotonUsuarios.php
@@ -1,5 +1,8 @@
 <?php 
+    include_once('../rutaAcciones.php');
     include_once('CreadorBoton.php');
+    include_once(DAO_PATH.'/BaseDeDatos.php');
+    include_once(DAO_PATH.'/TipoPersonaConsul.php');
 
     final class CrearBotonUsuarios extends CreadorBoton {
         public function __construct($idTipoPermiso) {
@@ -8,25 +11,53 @@
 
         public function crearBotones() {
             $botones = "";
+            $botones = $botones.'<h2 class="botones__titulo">Consultar</h2>';
+            $botones = $botones."<div class=\"input__grupo\">";
             if($this->permiso==4) {  //ADMINISTRADOR
-                $botones = $botones.'<h2 class="botones__titulo">Consultar</h2>';
-                $botones = $botones.$this->crearItemConsultatipoUsuario();
+                $botones = $botones.$this->crearItemConsultaTipoUsuario();
+                $botones = $botones.$this->crearItemConsultaTipoPersona();
             }
+            $botones = $botones.'</div>';
 
             return $botones;
         }
 
-        protected function crearItemConsultatipoUsuario() {
+        protected function crearItemConsultaTipoUsuario() {
             $item = 
             '<div class="input__grupo">';
-            // $item = $item.$this->crearItem("consultar", "Consultar");
             $item = $item.
                 '
-                <label for="tipoUsuarioInput" class="input__label">Tipo Usuario(s)</label>
+                <label for="tipoUsuarioInput" class="input__label">Validez</label>
                 <select class="input__select consultor" id="tipoUsuarioInput" name="tipoUsuarioInput">
-                    <option value="todos">todos</option>
-                    <option value="validos">validos</option>;
-                    <option value="invalidos">invalidos</option>';
+                    <option value="todos">Todos</option>
+                    <option value="validos">Validos</option>;
+                    <option value="invalidos">Invalidos</option>';
+            $item = $item.
+                '</select>
+            </div>';
+            return $item;
+        }
+
+        protected function crearItemConsultaTipoPersona() {
+            $item = 
+            '<div class="input__grupo">';
+            $item = $item.
+                '
+                <label for="tipoPersonaInput" class="input__label">Tipo Usuario</label>
+                <select class="input__select consultor" id="tipoPersonaInput" name="tipoPersonaInput">
+                    <option value="todos">Todos</option>';
+
+            $dao = new TipoPersonaConsul(BaseDeDatos::getInstancia());
+            $registros = $dao->getTodos();
+
+            for($i=0; $i<count($registros); $i++) {
+                $tipoPersona = $registros[$i];
+                $id = $tipoPersona->getId();
+                $descripcion = $tipoPersona->getDescripcion();
+                
+                $item = $item."<option value=\"$id\">$descripcion</option>";
+            }
+
             $item = $item.
                 '</select>
             </div>';
