6689548726ca3319ec29ce8fd386483d4c3da466
Andlicon <andresjzabalac@hotmail.com>
2023-02-27 19:37:35 -0400

    a

--- php/accion/componentes/creadores/input/CreadorInputPago.php
+++ php/accion/componentes/creadores/input/CreadorInputPago.php
@@ -25,37 +25,44 @@
             $html = 
                 "
                 <h2>Cargar pago</h2>
-                <div class=\"contenido__bloque\">
+                <div class=\"contenido__bloque\" style=\"display:flex; justify-content:center;\">
 	                <label>Elegir deuda</label>
 	                $selectDeuda
                 </div>
-                <div id=\"datos-deuda\">
-                
+            <div class=\"ladito\">
+                <div id=\"datos-deuda\" class=\"ocultar ladito--contenido\">
+            
                 </div>
-                <div class=\"marco-deuda\">
-                    <h4 class=\"popOver__informacion\">Pago a cargar<h4>
-                    <div class=\"contenido__bloque\">
-	                    <label for=\"fechaInput\">Fecha</label>
+                <div class=\"marco-deuda ladito--contenido\">
+                    <h4 class=\"popOver__informacion formu--titulo\">Pago a cargar</h4>
+                    <div class=\"formu\">
 	                    <input type=\"date\" id=\"fechaInput\">
+                        <span></span>
+                        <label for=\"fechaInput\">Fecha</label>
                     </div>
-                    <div class=\"contenido__bloque\">
-	                    <label for=\"montoInput\">Monto</label>
+                    <div class=\"formu\">
+                        <span></span>
 	                    <input type=\"text\" onkeypress=\"return soloNumeros(8, 'montoInput')\" id=\"montoInput\">
+                        <label for=\"montoInput\">Monto</label>
                     </div>
-                    <div class=\"contenido__bloque\">
+                    <div class=\"formu\">
+                        <span></span>
+                        $selectCuenta
 	                    <label for=\"cuentaInput\">Cuenta</label>
-	                    $selectCuenta
                     </div>
-                    <div class=\"contenido__bloque\">
+                    <div class=\"formu\">
+                        <span></span>
+                        $selectTipoPago
 	                    <label for=\"tipoPagoInput\">Tipo Pago</label>
-	                    $selectTipoPago
                     </div>
-                    <div class=\"contenido__bloque\">
-	                    <label for=\"ReferenciaInput\">Referencia</label>
+                    <div class=\"formu\">
+                        <span></span>
 	                    <input type=\"text\" onkeypress=\"return soloNumeros(8, 'referenciaInput')\" id=\"referenciaInput\">
+                        <label for=\"ReferenciaInput\">Referencia</label>
                     </div>
-                    <input class=\"boton btn\" id=\"cargar\" value=\"cargar\" />
                 </div>
+            </div>
+            <input class=\"boton btn\" id=\"cargar\" value=\"cargar\" />
                 <script>
                     $(document).ready(function () {
                         $('#deudaInput').select2();
@@ -67,6 +74,7 @@
 
                         if(deudaId==0) {
                             $('#datos-deuda').removeClass('marco-deuda');
+                            $('#datos-deuda').addClass('ocultar');
                             $('#datos-deuda').html('');
                         }
                         else {
@@ -88,14 +96,35 @@
                                     let nombreEstudiante = json[0].nombreEstudiante;
                                     let fecha = json[0].fecha;
                                 
-                                    $('#datos-deuda').html('<h4 class=\"popOver__informacion\">Informacion deuda<h4>'+
-                                                            '<p class=\"popOver__elemento\"><span class=\"negrita\">Estudiante: </span>'+nombreEstudiante+'</p>'+
-                                                           '<p class=\"popOver__elemento\"><span class=\"negrita\">Motivo: </span>'+motivo+'</p>'+
-                                                           '<p class=\"popOver__elemento\"><span class=\"negrita\">Descripcion: </span>'+descripcion+'</p>'+
-                                                           '<p class=\"popOver__elemento\"><span class=\"negrita\">Fecha: </span>'+fecha+'</p>'+
-                                                           '<p class=\"popOver__elemento\"><span class=\"negrita\">Deuda: </span>'+debe+'</p>'
+                                    $('#datos-deuda').html('<h4 class=\"popOver__informacion formu--titulo\">Información deuda seleccionada<h4>'+
+                                                            '<div class=\"formu\">'+
+                                                                '<input type=\"text\" value='+nombreEstudiante+' disabled>'+
+                                                                '<span></span>'+
+                                                                '<label class=\"negrita\">Estudiante</label>'+
+                                                            '</div>'+
+                                                            '<div class=\"formu\">'+
+                                                                '<input type=\"text\" value='+motivo+' disabled>'+
+                                                                '<span></span>'+
+                                                                '<label class=\"negrita\">Motivo</label>'+
+                                                            '</div>'+
+                                                            '<div class=\"formu\">'+
+                                                                '<input type=\"text\" value='+descripcion+' disabled>'+
+                                                                '<span></span>'+
+                                                                '<label class=\"negrita\">Descripcion</label>'+
+                                                            '</div>'+
+                                                            '<div class=\"formu\">'+
+                                                                '<input type=\"text\" value='+fecha+' disabled>'+
+                                                                '<span></span>'+
+                                                                '<label class=\"negrita\">Fecha</label>'+
+                                                            '</div>'+
+                                                            '<div class=\"formu\">'+
+                                                                '<input type=\"text\" value='+debe+' disabled>'+
+                                                                '<span></span>'+
+                                                                '<label class=\"negrita\">Deuda</label>'+
+                                                            '</div>'
                                                             );
                                     $('#datos-deuda').addClass('marco-deuda');
+                                    $('#datos-deuda').removeClass('ocultar');
                                 }
                             })
                         }
