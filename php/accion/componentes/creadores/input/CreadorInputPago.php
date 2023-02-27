bad2f2a0c845c5671421fc9470abe2e2fd3d4c27
Andlicon <andresjzabalac@hotmail.com>
2023-02-27 17:13:21 -0400

    a

--- php/accion/componentes/creadores/input/CreadorInputPago.php
+++ php/accion/componentes/creadores/input/CreadorInputPago.php
@@ -24,14 +24,15 @@
 
             $html = 
                 "
-                <input class=\"boton\" id=\"cargar\" value=\"cargar\" />
+                <h2>Cargar pago</h2>
                 <div class=\"contenido__bloque\">
-	                <label>Deuda</label>
+	                <label>Elegir deuda</label>
 	                $selectDeuda
                 </div>
                 <div id=\"datos-deuda\">
                 
                 </div>
+                <h4 class=\"popOver__informacion\">Pago a cargar<h4>
                 <div class=\"contenido__bloque\">
 	                <label for=\"fechaInput\">Fecha</label>
 	                <input type=\"date\" id=\"fechaInput\">
@@ -52,6 +53,7 @@
 	                <label for=\"ReferenciaInput\">Referencia</label>
 	                <input type=\"text\" onkeypress=\"return soloNumeros(8, 'referenciaInput')\" id=\"referenciaInput\">
                 </div>
+                <input class=\"boton\" id=\"cargar\" value=\"cargar\" />
 
                 <script>
                     $(document).ready(function () {
@@ -61,34 +63,39 @@
                 <script>    
                     $('#deudaInput').change(function() {
                         let deudaId = $('#deudaInput').val();
-                    
-                        $.ajax ( {
-                            url : '../../accion/consultar/cDeuda.php',
-                            type : 'POST',
-                            data : {deudaId: deudaId},
-                            async: false,
-                            success : function(response) {
-                                let json = JSON.parse(response);
-                            
-                                let id = json[0].id;
-                                let cedula = json[0].cedula;
-                                let descripcion = json[0].descripcion;
-                                let montoInicial = json[0].montoInicial;
-                                let montoEstado = json[0].montoEstado;
-                                let debe = json[0].debe;
-                                let motivo = json[0].motivo;
-                                let nombreEstudiante = json[0].nombreEstudiante;
-                                let fecha = json[0].fecha;
-                            
-                                $('#datos-deuda').html('<h4 class=\"popOver__informacion\">Informacion deuda a pagar<h4>'+
-                                                        '<p class=\"popOver__elemento\"><span class=\"negrita\">Estudiante: </span>'+nombreEstudiante+'</p>'+
-                                                       '<p class=\"popOver__elemento\"><span class=\"negrita\">Motivo: </span>'+motivo+'</p>'+
-                                                       '<p class=\"popOver__elemento\"><span class=\"negrita\">Descripcion: </span>'+descripcion+'</p>'+
-                                                       '<p class=\"popOver__elemento\"><span class=\"negrita\">Fecha: </span>'+fecha+'</p>'+
-                                                       '<p class=\"popOver__elemento\"><span class=\"negrita\">Deuda: </span>'+debe+'</p>'
-                                                        );
-                            }
-                        })
+
+                        if(deudaId==0) {
+                            $('#datos-deuda').html('');
+                        }
+                        else {
+                            $.ajax ( {
+                                url : '../../accion/consultar/cDeuda.php',
+                                type : 'POST',
+                                data : {deudaId: deudaId},
+                                async: false,
+                                success : function(response) {
+                                    let json = JSON.parse(response);
+                                
+                                    let id = json[0].id;
+                                    let cedula = json[0].cedula;
+                                    let descripcion = json[0].descripcion;
+                                    let montoInicial = json[0].montoInicial;
+                                    let montoEstado = json[0].montoEstado;
+                                    let debe = json[0].debe;
+                                    let motivo = json[0].motivo;
+                                    let nombreEstudiante = json[0].nombreEstudiante;
+                                    let fecha = json[0].fecha;
+                                
+                                    $('#datos-deuda').html('<h4 class=\"popOver__informacion\">Informacion deuda<h4>'+
+                                                            '<p class=\"popOver__elemento\"><span class=\"negrita\">Estudiante: </span>'+nombreEstudiante+'</p>'+
+                                                           '<p class=\"popOver__elemento\"><span class=\"negrita\">Motivo: </span>'+motivo+'</p>'+
+                                                           '<p class=\"popOver__elemento\"><span class=\"negrita\">Descripcion: </span>'+descripcion+'</p>'+
+                                                           '<p class=\"popOver__elemento\"><span class=\"negrita\">Fecha: </span>'+fecha+'</p>'+
+                                                           '<p class=\"popOver__elemento\"><span class=\"negrita\">Deuda: </span>'+debe+'</p>'
+                                                            );
+                                }
+                            })
+                        }
                     });
                 </script>
                 <script src=\"../js/limitador/soloNumeros.js\"></script>
